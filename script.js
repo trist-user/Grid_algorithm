69ab8614d4971ec83c4cc5591a8a2fb81f73c4f8
Tristan Owen <towen1@my.waketech.edu>
2026-04-16 14:07:08 -0400

    more progress, mi has grid con columns y rows

--- script.js
+++ script.js
@@ -7,16 +7,44 @@ class Grid{
     }
 
     createGrid(){
-        console.log("duh")
-        let gridTable = document.createElement("table") //undefined issues with mi elements
+        let gridTable = document.createElement("table") 
+        this.gridTable = gridTable
         gridTable.innerHTML = "dhdhdh"
-        console.log("table made")
         this.section.appendChild(gridTable);
-        for(let i=0; i<=this.xLength; i++){
+        for(let i=0; i<=this.xLength-1; i++){
             let gridRow = document.createElement("tr")
+            this.gridRow = gridRow
             gridRow.id = i
-            this.section.gridTable.appendChild(gridRow)
+            gridRow.innerHTML = i 
+            gridTable.appendChild(gridRow)
+            for(let j=0; j<=this.yLength-1; j++){
+                let gridColl = document.createElement("td")
+                this.gridColl = gridColl
+                gridColl.id = j
+                gridColl.innerHTML = j 
+                gridRow.appendChild(gridColl)
+            }
         }
 
     }
 }
+
+class GridSquare extends Grid{
+    constructor(targetGrid, squareType, coordX, coordY, xLength, yLength ){
+        super(xLength,yLength)
+        this.targetGrid = targetGrid
+        this.squareType = squareType
+        this.coordX = coordX
+        this.coordY = coordY
+        this.section = document.getElementById("grid")
+    }
+    locateSquare(){
+        let squareX = this.section.gridTable.getElementById("" +this.coordX)
+        let squareY = squareX.getElementById("" +this.coordY)
+        let targSquare = this.targetGrid.gridTable.squareX.squareY
+        return targSquare, squareX, squareY
+    }
+}
+
+y = 11232
+console.log("" + y) 
